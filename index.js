72360f91d4d5bd9ee6ad9e7102d39c28ddad4596
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-13 16:38:34 -0700

    Add conversion functionality for length, volume, and mass with updated HTML structure

--- index.js
+++ index.js
@@ -6,3 +6,28 @@
 
 const convertBtn = document.getElementById("convert-btn")
 const inputEl = document.getElementById("input-el")
+
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+
+convertBtn.addEventListener("click", function() {
+    let number = Number(inputEl.value)
+
+    // Length: 
+    let feet = number*3.281
+    let meter = number*0.3048
+    lengthEl.innerHTML = `${number} meters = ${feet} feet | ${number} feet = ${meter} meters `
+
+    // Volume:
+    let gallons = number*0.264
+    let liters = number*3.7854
+    volumeEl.innerHTML = `${number} liters = ${gallons} gallons | ${number} gallons = ${liters} liters `
+
+    // Mass: 
+    let pounds = number*2.204
+    let kilos = number*0.4536
+    massEl.innerHTML = `${number} kilos = ${pounds} pounds | ${number} pounds = ${kilos} kilos `
+
+})
